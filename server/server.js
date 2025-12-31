7cd434034af04c9b5d167bb8430eb4a310f56f48
Mohamed Nasir <Mohamednasir.s2006@gmail.com>
2025-12-31 09:45:15 +0530

    port

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 // Enable compression for all responses
 app.use(compression());
